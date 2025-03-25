0d70a369d538b6f79bb6ad2e954d95c5a8abeba7
ragaspace2004 <ragapriya.k2022cse@sece.ac.in>
2025-03-25 15:06:04 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import AddUser from './components/AddUser';
+// import NavBar from './components/NavBar';
+ import AddUser from './components/AddUser';
 
 function App() {
+   const handleAddUser = (userData) => {
+  console.log("User added:", userData);
+ };
   return (
     <div className="App">
-    <NavBar/>
-    <AddUser/>
+    <h1>Add User</h1>
+    <AddUser onAddUser={handleAddUser} />
     </div>
   );
 }
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,27 +1,50 @@
-import  { useForm }  from  "react-hook-form";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "../static/AddUser.css";
 
-function AddUser(){
-
-  const { register, handleSubmit, formState:{errors} } = useForm();
+function AddUser({ onAddUser }) {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
   const onSubmit = (data) => {
-    console.log(data);
+    setFormData(data); 
+    console.log(data); 
+    onAddUser(data); 
   };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Username</label>
-      <input type="text" {...register("username", { required: true })} />
+      <input
+        type="text"
+        {...register("username", { required: true })}
+        onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+      />
       {errors.username && <p>Username is required</p>}
+
       <label>Email</label>
-      <input type="email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+      <input
+        type="email"
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      />
       {errors.email && <p>Email is required and must be valid</p>}
 
       <label>Password</label>
-      <input type="password" {...register("password", { required: true })} />
+      <input
+        type="password"
+        {...register("password", { required: true })}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+      />
       {errors.password && <p>Password is required</p>}
 
-      <button type="submit">Submit</button>
+      <button type="submit">Add User</button>
     </form>
   );
 }
+
 export default AddUser;
